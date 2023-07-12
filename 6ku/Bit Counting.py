a73d98360bc476611084bace8dcc02a6cc1620d4
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-07-12 14:43:34 +0100

    Added first codewars problem

---
+++ 6ku/Bit Counting.py
@@ -0,0 +1,21 @@
+# Bit Counting: 
+# Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+# Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+## My Solution: 
+
+def count_bits(n):
+    count = 0
+    binary = format(n , 'b')
+    for element in binary:
+        if element == '1':
+            count += 1
+        else:
+            continue
+    return count
+
+
+# Best Solution Found:
+
+def countBits(n):
+    return bin(n).count("1")
