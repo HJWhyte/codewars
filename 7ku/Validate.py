c0317758342ba9de59023ce125df72503b102509
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-07-18 13:14:30 +0100

    18/07/23 - Problem 11 - 7ku

---
+++ 7ku/Validate.py
@@ -0,0 +1,23 @@
+# Validate
+
+# DESCRIPTION:
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+# If the function is passed a valid PIN string, return true, else return false.
+
+# Examples (Input --> Output)
+# "1234"   -->  true
+# "12345"  -->  false
+# "a234"   -->  false
+
+# My solution:
+
+def validate_pin(pin):
+    if (len(pin) == 4 or len(pin) == 6) and pin.isnumeric() == True:
+        return True
+    else: 
+        return False
+
+# Best found solution:
+
+def validate_pin(pin):
+    return len(pin) in (4, 6) and pin.isdigit()
